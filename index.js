5396419eae1e2ca5badd2977bcbf8dabcbe6d87e
pervejislam2404 <pervejislam2404@gmail.com>
2021-10-27 02:09:23 +0600

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+app.use(cors());
+app.use(express.json());
+
+const port = 2050;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qvyqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("volunteer-service");
+        const volunteerCollection = database.collection("volunteers");
+        app.get('/services', async(req, res) => {
+            res.send('server run success')
+        })
+    } finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+app.listen(port, () => {
+    console.log('server running on port ' + port);
+})
